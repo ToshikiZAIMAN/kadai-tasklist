cd8edaae3e4fe43078f37f72509bea221c5aee97
toshiki.zaiman <bswill2a92@gmail.com>
2017-04-18 06:00:07 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
 
-    if @task.update(message_params)
+    if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
     else
@@ -46,4 +46,10 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
+  
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
 end
